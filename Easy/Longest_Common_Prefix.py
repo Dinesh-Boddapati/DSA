927c3024ea4d9258519d4a5344c73c10fb7bdcac
Dinesh Boddapat <dineshboddapati69@gmail.com>
2025-10-28 18:33:53 -0500

    Day 3

--- Easy/Longest_Common_Prefix.py
+++ Easy/Longest_Common_Prefix.py
@@ -25,7 +25,7 @@
 
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
        prefix=""
        
        for i in range(len(strs[0])):
